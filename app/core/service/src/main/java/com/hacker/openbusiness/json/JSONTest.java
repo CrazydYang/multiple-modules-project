8fd8a401d7477a93de3c065a5429f2933a906a85
fuyuanhui <18368116334@613.com>
2019-04-26 01:14:25 +0800

    这个是一个工程架构，里面没有具体的代码，只是演示如何构建maven的多模块项目，里面对maven的各个标签，以及如何依赖等做了一些说明

---
+++ app/core/service/src/main/java/com/hacker/openbusiness/json/JSONTest.java
@@ -0,0 +1,21 @@
+package com.hacker.openbusiness.json;
+
+import com.alibaba.fastjson.JSONObject;
+
+/**
+ * Created by hacker on 2019/4/26 0026-上午 12:46
+ *
+ * @desc
+ */
+public class JSONTest {
+
+    public static void main(String[] args) {
+
+        /**
+         * 当前子模块不引入任何依赖，只在父工程中引入就可以使用
+         * 注意，这里有一个前提哦，就是不能使用
+         */
+        JSONObject jsonObject = new JSONObject(1);
+
+    }
+}
